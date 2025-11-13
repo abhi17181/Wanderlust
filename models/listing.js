b863c97834c3bf89d3ac785a6aa7ee3f8a8bac5c
abhishek18 <as7651173@gmail.com>
2025-11-13 18:27:00 +0530

    initializing repo

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true,
+    }, 
+    description:String,
+    image: {
+        filename: { type: String, default: "listingimage" },
+        url: {
+            type: String, 
+            default: "https://unsplash.com/photos/sunlight-streams-through-trees-onto-a-coastal-cliffside-Xh4VxC5br-s"
+        }
+    },
+    price:Number,
+    location:String,
+    country:String
+});
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;

96b741d0e34544b320c28e574a3dd359d86d4b7f
abhishek18 <as7651173@gmail.com>
2025-11-18 23:31:30 +0530

    Updated

--- routes/listing.js
+++ routes/listing.js
@@ -39,12 +39,13 @@ router.get("/:id",wrapAsync(async(req,res)=>{
 router.post("/",validateListing,wrapAsync(async(req,res,next)=>{
     const newListing=new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success","New Listing created!");
     res.redirect("/listings");
 }));
 //Edit Route
 router.get("/:id/edit",wrapAsync(async(req,res)=>{
     let {id}=req.params;
-    const listing=await Listing.findById(id);
+    const listing=await Listing.findById(id);   
     res.render("listings/edit.ejs",{listing});
 }));
 
@@ -52,6 +53,7 @@ router.get("/:id/edit",wrapAsync(async(req,res)=>{
 router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    req.flash(("success","Listing Updated!"));
     res.redirect(`/listings/${id}`);
 }));
 
@@ -60,6 +62,7 @@ router.delete("/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash(("success","Listing deleted!"));
     res.redirect("/listings");
 }));
 module.exports=router;
